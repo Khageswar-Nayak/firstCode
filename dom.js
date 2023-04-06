853979f718358cc0a6bb01868cbc8c098fa6085f
Khageswar-Nayak <khageswarnayak045@gmail.com>
2023-04-06 12:13:36 +0530

    TEST 3

--- dom.js
+++ dom.js
@@ -7,11 +7,11 @@ headerTitle.style.borderBottom = 'solid 3px #000';
 
 var addItem = document.getElementsByClassName('title');
 addItem[0].style.color = 'green';
-console.log(addItem);
+//console.log(addItem);
 //document.all[14].style.color = 'green';
 
 var items= document.getElementsByClassName('list-group-item');
-//console.log(items[2]);
+//console.log(items);
 items[2].style.backgroundColor = 'green';
 //items.style.backgroundColor ='green';
 
@@ -19,3 +19,12 @@ for(var i=0; i<items.length; i++)
 {
     items[i].style.fontWeight='bold';
 }
+var li= document.getElementsByClassName('list-outside-item');
+//console.log(li);
+li[0].style.color = 'red';
+ //items.style.backgroundColor ='green';
+
+// for(var i=0; i<li.length; i++)
+// {
+//     li[i].style.fontWeight='bold';
+// }
