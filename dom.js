3a47f21941ceaa92337dcf4db3dcb472d1af0c31
Khageswar-Nayak <khageswarnayak045@gmail.com>
2023-04-06 15:46:12 +0530

    TEST 4

--- dom.js
+++ dom.js
@@ -12,19 +12,31 @@ addItem[0].style.color = 'green';
 
 var items= document.getElementsByClassName('list-group-item');
 //console.log(items);
-items[2].style.backgroundColor = 'green';
+// items[2].style.backgroundColor = 'green';
 //items.style.backgroundColor ='green';
 
 for(var i=0; i<items.length; i++)
 {
     items[i].style.fontWeight='bold';
 }
-var li= document.getElementsByClassName('list-outside-item');
-//console.log(li);
-li[0].style.color = 'red';
+// var li= document.getElementsByClassName('list-outside-item');
+// //console.log(li);
+// li[0].style.color = 'red';
  //items.style.backgroundColor ='green';
 
 // for(var i=0; i<li.length; i++)
 // {
 //     li[i].style.fontWeight='bold';
 // }
+// var secondItem=document.querySelector('.list-group-item:nth-child(3)');
+// secondItem.style.display = 'none';
+
+var items= document.querySelectorAll('.list-group-item');
+//console.log(items);
+items[1].style.color='green';
+
+var odd= document.querySelectorAll('li:nth-child(odd)');
+
+for(var i=0; i<odd.length; i++){
+    odd[i].style.backgroundColor='green';
+}
