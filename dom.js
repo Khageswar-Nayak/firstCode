929b1868cc2a76414444f2f8282386ba7e50e09d
Khageswar-Nayak <khageswarnayak045@gmail.com>
2023-04-08 14:53:23 +0530

    FILTER TEST

--- dom.js
+++ dom.js
@@ -1,13 +1,13 @@
+
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
-var filter = document.getElementById('filter');
+
 
 // Form submit event
 form.addEventListener('submit', addItem);
 
 
-// // Filter event
-// filter.addEventListener('keyup', filterItems);
+
 
 // Add item
 function addItem(e){
@@ -15,14 +15,14 @@ function addItem(e){
 
   // Get input value
   var newItem = document.getElementById('item').value;
-
+  var newInput = document.getElementById('description').value;
   // Create new li element
   var li = document.createElement('li');
   // Add class
   li.className = 'list-group-item';
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
-
+  li.appendChild(document.createTextNode(newInput));
   //Create del button element
   var deleteBtn = document.createElement('button');
 
@@ -58,11 +58,35 @@ function removeItem(e){
   
   let items=document.getElementsByClassName('list-group-item');
   for(var i=0; i<items.length; i++){
-    console.log("hii");
-    console.log(items[i]);
+    // console.log("hii");
+    // console.log(items[i]);
     let editBtn = document.createElement('button');
-  editBtn.className = 'btn btn-success btn-sm float-right';
+  editBtn.className = 'btn btn-success btn-sm float-right mr-1';
   editBtn.appendChild(document.createTextNode('Edit'));
     items[i].append(editBtn);
   }
   
+  var filter = document.getElementById('filter');
+  // Filter event
+ filter.addEventListener('keyup', filterItems);
+
+ // Filter Items
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // console.log(text);
+    // Get lis
+    var items = itemList.getElementsByTagName('li');
+    //console.log(items);
+    // // Convert to an array
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      let description = item.childNodes[1].textContent;
+
+      if(itemName.toLowerCase().indexOf(text) != -1 || description.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
