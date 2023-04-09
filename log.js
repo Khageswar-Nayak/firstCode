75941ab878e76f0b0133c47d5ff4948c5defe956
Khageswar-Nayak <khageswarnayak045@gmail.com>
2023-04-09 17:06:30 +0530

    Storing obj in LocalStorage

--- log.js
+++ log.js
@@ -3,10 +3,14 @@ document.addEventListener('submit', addItem);
 function addItem(e){
     e.preventDefault();
     var fullname = document.getElementById('name').value;
-    localStorage.setItem('name', fullname);
-
+    
     var email = document.getElementById('email').value;
-    localStorage.setItem('email', email);
+
+    let obj={
+        name :fullname,
+        email:email
+    }
+    localStorage.setItem('userDetails', JSON.stringify(obj));
 }
 // var firstName = document.getElementById('name').value;
 // console.log(firstName);
